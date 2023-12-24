0a301f40358626eed36241a47842a8a55e44bed5
olamilekan <issaabubakry@gmail.com>
2023-12-24 22:18:41 +0100

    created a function for search option

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,22 @@
 /**
  * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright 2023 codewithAbella
+ * @author codewithAbella <issaabubakry@gmail.com>
  */
 
 "use strict";
+
+/**
+ * Home page search
+ */
+
+const /** {NodeElement} */ $searchField = document.querySelector("[data-search-field]");
+const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click", function () {
+    if ($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+/**
+ * Search submit when pressing "Enter"
+ */
