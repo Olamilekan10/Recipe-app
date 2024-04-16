4ac0052c69b43be48bc49170e8594f43d77f35eb
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-04-16 13:00:16 -0700

    continuation on the recipe app by creating the dish selection option section

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,20 @@
 /**
  * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright 2023 Abella
+ * @author Abella <issaabubakry@gmail.com>
  */
 
 "use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {Nodelist} $elements NodeList
+ * @param {String} $eventType Event type string
+ * @param {function} callback callback function
+ */
+
+window.addEventOnElements = ($elements, eventType, callback) => {
+    for (const $element of $elements) {
+        $element.addEventListener(eventType, callback);
+    }
+}
--- assets/js/home.js
+++ assets/js/home.js
@@ -32,3 +32,43 @@ $searchField.addEventListener("keydown", e => {
 
 const /** {NodeList} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
 const /** {NodeList} */ $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let /** NodeElement */ [$lastActiveTabPanel] = $tabPanels;
+let /** NodeElement */ [$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, "click", function () {
+    $lastActiveTabPanel.setAttribute("hidden", "");
+    $lastActiveTabBtn.setAttribute("arial-selected", false);
+    $lastActiveTabBtn.setAttribute("tabindex", -1);
+
+    const /** {NodeList} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden");
+    this.setAttribute("aria-selected", true);
+    this.setAttribute("tabindex", 0);
+
+    $lastActiveTabPanel = $currentTabPanel;
+    $lastActiveTabBtn = this;
+});
+
+/**
+ * Navigate Tab with arrow key
+ */
+
+addEventOnElements($tabBtns, "keydown", function(e) {
+    const /** {NodeElement} */ $nextElement = this.nextElementSibling;
+    const /** {NodeElement} */ $previousElement = this.previousElementSibling;
+
+    if (e.key === "ArrowRight" && $nextElement) {
+        this.setAttribute("tabindex", -1);
+        $nextElement.setAttribute("tabindex", 0);
+        $nextElement.focus();
+    } else if (e.key === "ArrowLeft" && $previousElement) {
+        this.setAttribute("tabindex", -1);
+        $previousElement.setAttribute("tabindex", 0);
+        $previousElement.focus();
+    } else if (e.key === "Tab") {
+        this.setAttribute("tabindex", -1);
+        $lastActiveTabBtn.setAttribute("tabindex", 0);
+    }
+    
+});
