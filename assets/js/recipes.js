eb6b44c7037532f66d091e41fc31c6ce7c76b164
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-06-05 10:17:19 -0700

    creating a filter bar toggler for mobile screen

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -23,20 +23,32 @@ import { getTime } from "./module.js";
 const /** {NodeList} */ $accordions = document.querySelectorAll("[data-accordion]");
 
 /**
- * 
  * @param {NodeList} $element Accordion node
- */
+*/
 
 const initAccordion = function ($element) {
-
+    
     const /** {NodeList} */ $button = $element.querySelector("[data-accordion-btn]");
     let isExpanded = false; 
-
+    
     $button.addEventListener("click", function () {
         isExpanded = isExpanded ? false : true;
         this.setAttribute("aria-expanded", isExpanded);
     });
-
+    
 } 
 
 for (const $accordion of $accordions) initAccordion($accordion);
+
+
+/**
+ * Filter bar toggle for mon=bile screen
+*/
+
+const /** {NodeElement} */ $filterBar = document.querySelector("[data-filter-bar]");
+const /** {NodeList} */ $filterTogglers = document.querySelectorAll("[data-filter-toggler]");
+const /** {NodeElement} */ $overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements($filterTogglers, "click", function () {
+    $filterBar.
+});
