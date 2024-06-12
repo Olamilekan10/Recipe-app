1ef77dcad74c944f916a10f8434b04b282a5561d
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-06-11 18:37:31 -0700

    wrote a script to get the saved recipefrom local storage and show in browser console

--- assets/js/saved_recipes.js
+++ assets/js/saved_recipes.js
@@ -18,4 +18,25 @@ const /** {Array} */ savedRecipes = Object.keys(window.localStorage).filter(item
     return item.startsWith("cookio-recipe");
 });
 
-console.log(savedRecipes)
+const /** {NodeElement} */ $savedRecipeContainer = document.querySelector("[data-saved-recipe-container]");
+
+$savedRecipeContainer.innerHTML = `<h2 class="headline-small section-title">All Saved Recipes</h2>`;
+const /** {NodeElement} */ $gridList = document.createElement("div");
+$gridList.classList.add("grid-list");
+
+if (savedRecipes.length) {
+    savedRecipes.map((savedRecipe, index) => {
+
+        const {
+            recipe : {
+                image,
+                label: title,
+                totalTime: cookingTime,
+                uri
+            }
+        } = JSON.parse(window.localStorage.getItem(savedRecipe));
+
+        console.log(JSON.parse(window.localStorage.getItem(savedRecipe)));
+
+    });
+}
